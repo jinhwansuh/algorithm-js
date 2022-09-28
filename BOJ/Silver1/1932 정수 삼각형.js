2618fd7656685ce3425e15c017214e266b654ddc
jinhwansuh <nick02190219@gmail.com>
2022-09-28 15:25:08 +0900

    백준 1932 정수 삼각형 dp

--- BOJ/Silver1/1932 정수 삼각형.js
+++ BOJ/Silver1/1932 정수 삼각형.js
@@ -8,6 +8,23 @@ const input = array.map((arr) =>
     .map((el) => parseInt(el, 10))
 );
 
+{
+  function solution(input) {
+    for (let i = 1; i < input.length; i++) {
+      for (let j = 0; j <= i; j++) {
+        let prev;
+        if (j === 0) prev = input[i - 1][j];
+        else if (j === i) prev = input[i - 1][j - 1];
+        else prev = Math.max(input[i - 1][j - 1], input[i - 1][j]);
+        input[i][j] += prev;
+      }
+    }
+
+    return Math.max(...input[input.length - 1]);
+  }
+  console.log(solution(input));
+}
+
 // dfs 시간 초과
 {
   function solution(input) {
